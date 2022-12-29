fe633a4bba2f5f97c95d60d04c8614ad3dadb468
Anastasia Polivoda <polivoda.anastasiia@gmail.com>
2022-12-30 02:50:42 +0300

    fix: url

--- src/server/post.tsx
+++ src/server/post.tsx
@@ -3,7 +3,7 @@ import { requests } from "./constants";
 
 export const POST = async (n: number, body?: ITile[]) => {
   const { TYPE, POST } = requests;
-  const request = await fetch(`https://hex2048-lambda.octa.wtf/${n}`, {
+  const request = await fetch(`http://localhost:13337/${n}`, {
     method: `${POST}`,
     headers: {
       Accept: `${TYPE}`,
