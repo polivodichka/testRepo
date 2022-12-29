a9c0b128ca218a7b9fcae4891c1687241e9cc348
Anastasia Polivoda <polivoda.anastasiia@gmail.com>
2022-12-30 00:23:38 +0300

    refactor: imports order

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import "./index.css";
 
-import App from "./components/App";
-import { store } from "./store/store";
-
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+
+import { store } from "./store/store";
+import App from "./components/App";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
